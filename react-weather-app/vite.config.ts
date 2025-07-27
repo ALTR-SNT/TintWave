4c775dc1d7e472e366190b725629a44d5b0c4ced
Saint Yan <varga.yanosh.05@gmail.com>
2025-07-27 19:58:45 +0300

    fix + added PWA

--- react-weather-app/vite.config.ts
+++ react-weather-app/vite.config.ts
@@ -1,10 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),  
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true, 
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+    name: "TintWave PWA",
+    short_name: "TintWave",
+    description: "Weather App",
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+    start_url: "/",
+    icons: [
+      {
+        src: "sun192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "sun512.png",
+        sizes: "512x512",
+        type: "image/png"
+      },
+      {
+        src: "sun.svg",
+        sizes: "any",
+        type: "image/svg+xml",
+        purpose: "maskable"
+      }
+    ]
+  },
+    }),
   ],
 })
